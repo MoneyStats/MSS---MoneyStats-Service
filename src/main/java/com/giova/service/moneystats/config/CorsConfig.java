656d1796eb41c3e39bb60d6ddbbce464c1ecdc0b
Giovanni Lamarmora <giovannilamarmora.working@gmail.com>
2023-04-06 22:51:11 +0200

    fix(): Auth-Token Header

--- src/main/java/com/giova/service/moneystats/config/CorsConfig.java
+++ src/main/java/com/giova/service/moneystats/config/CorsConfig.java
@@ -18,6 +18,7 @@ public class CorsConfig implements Filter {
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Expose-Headers", "*");
     response.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization, authToken, authtoken");
