d96b4766eb59875714967c7db7bc41fe17e0e9db
Giovanni <giovannilamarmora.working@gmail.com>
2023-07-25 17:57:54 +0200

    feat(): webClientRest bump 1.9.1

--- src/main/java/com/giova/service/moneystats/api/coingecko/CoinGeckoClient.java
+++ src/main/java/com/giova/service/moneystats/api/coingecko/CoinGeckoClient.java
@@ -3,13 +3,23 @@ package com.giova.service.moneystats.api.coingecko;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.giova.service.moneystats.api.coingecko.dto.CoinGeckoMarketData;
+import io.github.giovannilamarmora.utils.interceptors.LogInterceptor;
+import io.github.giovannilamarmora.utils.interceptors.LogTimeTracker;
 import io.github.giovannilamarmora.utils.interceptors.Logged;
+import io.github.giovannilamarmora.utils.webClient.UtilsUriBuilder;
+import io.github.giovannilamarmora.utils.webClient.WebClientRest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 @Component
 @Logged
@@ -17,14 +27,21 @@ public class CoinGeckoClient {
 
   private final RestTemplate restTemplate = new RestTemplate();
   private final ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
-
+  private final WebClientRest webClientRest = new WebClientRest();
   @Value(value = "${rest.client.coinGecko.url}")
   private String coinGeckoUrl;
-
   @Value(value = "${rest.client.coinGecko.marketDataUrl}")
   private String marketDataUrl;
+  @Autowired private WebClient.Builder builder;
 
-  public ResponseEntity<List<CoinGeckoMarketData>> getMarketData(String currency) {
+  @PostConstruct
+  void init() {
+    webClientRest.setBaseUrl(coinGeckoUrl);
+    webClientRest.init(builder);
+  }
+
+  @LogInterceptor(type = LogTimeTracker.ActionType.APP_EXTERNAL)
+  public ResponseEntity<List<CoinGeckoMarketData>> getMarketDataRest(String currency) {
     HttpHeaders headers = new HttpHeaders();
     headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
     HttpEntity<?> request = new HttpEntity<>(headers);
@@ -42,6 +59,33 @@ public class CoinGeckoClient {
         restTemplate.exchange(urlTemplate, HttpMethod.GET, request, Object.class);
     List<CoinGeckoMarketData> list =
         mapper.convertValue(response.getBody(), new TypeReference<List<CoinGeckoMarketData>>() {});
+
+    return ResponseEntity.ok(list);
+  }
+
+  @LogInterceptor(type = LogTimeTracker.ActionType.APP_EXTERNAL)
+  public ResponseEntity<List<CoinGeckoMarketData>> getMarketData(String currency) {
+    Map<String, Object> params = new HashMap<>();
+    params.put("vs_currency", currency);
+    params.put("order", "market_cap_desc");
+    params.put("per_page", 200);
+    params.put("page", 1);
+    params.put("sparkline", false);
+    params.put("locale", "it");
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+    Mono<ResponseEntity<Object>> response =
+        webClientRest.perform(
+            HttpMethod.GET,
+            UtilsUriBuilder.toBuild().set(marketDataUrl, params),
+            headers,
+            Object.class);
+
+    List<CoinGeckoMarketData> list =
+        mapper.convertValue(
+            response.block().getBody(), new TypeReference<List<CoinGeckoMarketData>>() {});
 
     return ResponseEntity.ok(list);
   }
