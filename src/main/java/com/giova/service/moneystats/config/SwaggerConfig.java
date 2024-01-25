b99201bffb0cd1ac1f1cf8b147775c15e43668b0
Giovanni <giovannilamarmora.working@gmail.com>
2024-01-25 16:28:58 +0100

    feat(): Bug Fixed, added SwaggerConfig, Bump 1.16.45

--- src/main/java/com/giova/service/moneystats/config/SwaggerConfig.java
+++ src/main/java/com/giova/service/moneystats/config/SwaggerConfig.java
@@ -1,23 +1,31 @@
 package com.giova.service.moneystats.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.Paths;
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.Collections;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.ObjectUtils;
 
 @Configuration
 public class SwaggerConfig {
 
   private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+  private final ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
+  @Autowired private ResourceLoader resourceLoader;
 
   @Bean
   public OpenApiCustomiser applyStandardOpenAPIModifications() {
@@ -85,20 +93,37 @@ public class SwaggerConfig {
   }
 
   private String getFilePathFromResources(String fileName) throws IOException {
-    try (Stream<Path> paths = Files.walk(Path.of("src/main/resources"))) {
-      return paths
-          .filter(path -> path.toFile().isFile() && path.toString().endsWith(fileName))
-          .findFirst()
-          .map(
-              path -> {
-                try {
-                  return new String(Files.readAllBytes(path));
-                } catch (IOException e) {
-                  LOG.error("An Exception occurred during reading file", e);
-                  return null;
-                }
-              })
-          .orElse(null);
+    Path path = getResourcePath(fileName);
+    LOG.debug("The Path is {}", path);
+    return path != null ? new String(Files.readAllBytes(path)) : null;
+  }
+
+  private Path getResourcePath(String fileName) throws IOException {
+    Resource resource = resourceLoader.getResource("classpath:");
+
+    LOG.debug("The Resource URI is {}", resource.getURI());
+
+    Path resourcesPath;
+
+    if (resource.getURI().getScheme().equals("jar")) {
+      FileSystem fileSystem = FileSystems.newFileSystem(resource.getURI(), Collections.emptyMap());
+      resourcesPath = fileSystem.getPath("/BOOT-INF/classes/");
+      LOG.debug(
+          "The Resource Path for scheme {} is {}",
+          resource.getURI().getScheme(),
+          resourcesPath.toUri());
+    } else {
+      resourcesPath = Path.of(resource.getURI());
+      LOG.debug(
+          "The Resource Path for scheme {} is {}",
+          resource.getURI().getScheme(),
+          resourcesPath.toUri());
+    }
+
+    try (Stream<Path> paths = Files.walk(resourcesPath)) {
+      Predicate<Path> validatePath =
+          path -> path != null && path.toFile().isFile() && path.toString().endsWith(fileName);
+      return paths.filter(validatePath).findFirst().orElse(null);
     }
   }
 }
