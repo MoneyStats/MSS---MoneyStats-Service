139bf83931c3eeac9ff59e06310a3ca2056fc757
Giovanni <giovannilamarmora.working@gmail.com>
2024-02-09 21:02:52 +0100

    feat(): CORS enabled, Update Utils, Bump 1.16.73

--- src/main/java/com/giova/service/moneystats/config/AppConfig.java
+++ src/main/java/com/giova/service/moneystats/config/AppConfig.java
@@ -2,27 +2,15 @@ package com.giova.service.moneystats.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.giova.service.moneystats.authentication.entity.UserEntity;
+import io.github.giovannilamarmora.utils.config.OpenAPIConfig;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.Operation;
-import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.Paths;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.Enumeration;
 import java.util.Map;
-import java.util.function.Predicate;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springdoc.core.customizers.OpenApiCustomiser;
@@ -31,10 +19,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.util.ObjectUtils;
 
 @ComponentScan(basePackages = "io.github.giovannilamarmora.utils")
 @Configuration
@@ -42,8 +29,11 @@ import org.springframework.util.ObjectUtils;
 @EnableCaching
 @OpenAPIDefinition(
     info = @Info(title = "MoneyStats Swagger", version = "1.0.0"),
-    security = {@SecurityRequirement(name = "JWE Token")})
-@SecurityScheme(type = SecuritySchemeType.APIKEY, name = "JWE Token", in = SecuritySchemeIn.HEADER)
+    security = {@SecurityRequirement(name = HttpHeaders.AUTHORIZATION)})
+@SecurityScheme(
+    type = SecuritySchemeType.APIKEY,
+    name = HttpHeaders.AUTHORIZATION,
+    in = SecuritySchemeIn.HEADER)
 public class AppConfig {
 
   private final Logger LOG = LoggerFactory.getLogger(this.getClass());
@@ -63,126 +53,10 @@ public class AppConfig {
           .sorted(Map.Entry.comparingByKey())
           .forEach(
               entry ->
-                  paths.addPathItem(entry.getKey(), addExamples(entry.getValue(), resourceLoader)));
+                  paths.addPathItem(
+                      entry.getKey(),
+                      OpenAPIConfig.addJSONExamplesOnResource(entry.getValue(), resourceLoader)));
       openApi.setPaths(paths);
     };
-  }
-
-  public PathItem addExamples(PathItem pathItem, ResourceLoader resourceLoader) {
-    if (!ObjectUtils.isEmpty(pathItem.getGet())) {
-      addOperations(pathItem.getGet(), resourceLoader);
-    }
-    if (!ObjectUtils.isEmpty(pathItem.getPost())) {
-      addOperations(pathItem.getPost(), resourceLoader);
-    }
-    if (!ObjectUtils.isEmpty(pathItem.getPatch())) {
-      addOperations(pathItem.getPatch(), resourceLoader);
-    }
-    if (!ObjectUtils.isEmpty(pathItem.getPut())) {
-      addOperations(pathItem.getPut(), resourceLoader);
-    }
-    if (!ObjectUtils.isEmpty(pathItem.getDelete())) {
-      addOperations(pathItem.getDelete(), resourceLoader);
-    }
-    return pathItem;
-  }
-
-  private void addOperations(Operation operation, ResourceLoader resourceLoader) {
-    if (!ObjectUtils.isEmpty(operation) && !ObjectUtils.isEmpty(operation.getResponses())) {
-      operation
-          .getResponses()
-          .forEach(
-              (statusCode, apiResponse) -> {
-                if (!ObjectUtils.isEmpty(apiResponse.getContent())) {
-                  apiResponse
-                      .getContent()
-                      .values()
-                      .forEach(
-                          content -> {
-                            try {
-                              if (ObjectUtils.isEmpty(content.getExample())) return;
-                              String fileName = getExampleFileName(content.getExample().toString());
-                              LOG.debug(
-                                  "Content {} and FileName {}", content.getExample(), fileName);
-                              String jsonContent =
-                                  getFilePathFromResources(fileName, resourceLoader);
-                              if (jsonContent != null) {
-                                content.setExample(jsonContent);
-                              }
-                            } catch (Exception e) {
-                              LOG.error(
-                                  "An Exception occurred during read filename for Open API", e);
-                            }
-                          });
-                }
-              });
-    }
-  }
-
-  private String getExampleFileName(String fileName) {
-    return fileName.replaceFirst("@", "");
-  }
-
-  private String getFilePathFromResources(String fileName, ResourceLoader resourceLoader)
-      throws IOException {
-    Path path = getResourcePath(fileName, resourceLoader);
-    LOG.debug("The Path is {}", path);
-    return path != null ? new String(Files.readAllBytes(path)) : null;
-  }
-
-  private Path getResourcePath(String fileName, ResourceLoader resourceLoader) throws IOException {
-    Resource resource = resourceLoader.getResource("classpath:/");
-
-    LOG.debug("The Resource URI is {}", resource.getURI());
-
-    Path resourcesPath;
-
-    if (resource.getURI().getScheme().equals("jar")) {
-      URL resourceUrl = AppConfig.class.getProtectionDomain().getCodeSource().getLocation();
-      LOG.debug(
-          "The Resource Path for scheme {} is {} and jar path {}, resource url {}",
-          resource.getURI().getScheme(),
-          resource.getURI(),
-          resource.getURI().getPath(),
-          resourceUrl);
-      String path = resourceUrl.getPath();
-      if (path.endsWith("/")) {
-        // Remove trailing "/"
-        path = path.substring(0, path.length() - 1);
-      }
-      String jarPath = path.substring(5, path.indexOf("!"));
-
-      LOG.debug("JarPath {}", jarPath);
-
-      try (JarFile jarFile = new JarFile(jarPath)) {
-        Enumeration<JarEntry> entries = jarFile.entries();
-        while (entries.hasMoreElements()) {
-          JarEntry entry = entries.nextElement();
-          if (!entry.isDirectory() && entry.getName().endsWith(fileName)) {
-            // Found the entry matching the folder and file name
-            try (InputStream entryStream = jarFile.getInputStream(entry)) {
-              Path tempFile = Files.createTempFile("jar-entry", null);
-              Files.copy(entryStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
-              LOG.debug(
-                  "The Resource Path for scheme {} is {}", resource.getURI().getScheme(), tempFile);
-              return tempFile;
-            }
-          }
-        }
-      }
-    } else {
-      resourcesPath = Path.of(resource.getURI());
-      LOG.debug(
-          "The Resource Path for scheme {} is {}",
-          resource.getURI().getScheme(),
-          resourcesPath.toUri());
-
-      try (Stream<Path> paths = Files.walk(resourcesPath)) {
-        Predicate<Path> validatePath =
-            path -> path != null && path.toFile().isFile() && path.toString().endsWith(fileName);
-        return paths.filter(validatePath).findFirst().orElse(null);
-      }
-    }
-    return null;
   }
 }
